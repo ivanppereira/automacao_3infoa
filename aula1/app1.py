69f61e496f14f5f82508b8e25aed758e4019fe2c
Ivan Pereira <ivan.pereira@ifsuldeminas.edu.br>
2026-03-30 09:18:44 -0300

    atualização da aula 3

---
+++ aula1/app1.py
@@ -0,0 +1,8 @@
+#comando de entrada + conversão de dados + variaveis
+idade = int(input("Digite a sua idade: "))
+
+#processamento + operador aritimético
+idade = idade + 1
+
+#comando de saída
+print("No ano que vem você terá ", idade, 'anos') 
